e408627ec1249e8aaf91b498921e3fe90b8924cc
João_Fidelis <jfidelis999@gmail.com>
2025-02-10 20:12:27 -0300

    helloword

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        calcular calc = new calcular();
+        int resultado = calc.calcular(8, 9);
+        System.out.println("Hello, World! :)" + resultado);
+    }
+}
---
+++ HelloWorld/src/calcular.java
@@ -0,0 +1,5 @@
+public class calcular {
+   public static int calcular(int a, int b){
+        return a + b;
+   }
+}

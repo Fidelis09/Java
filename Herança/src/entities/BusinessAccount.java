cb60a750fe5e77f847a1591f6fe4c55a3acee3e0
João_Fidelis <jfidelis999@gmail.com>
2025-03-07 13:40:39 -0300

    herança

---
+++ Herança/src/entities/BusinessAccount.java
@@ -0,0 +1,30 @@
+package entities;
+
+
+
+public class BusinessAccount extends Account {
+
+    private Double loanLimit;
+
+    public BusinessAccount(Integer number, String holder, double balance, Double loanLimit) {
+        super(number, holder, balance);
+        this.loanLimit = loanLimit;
+    }
+
+    public BusinessAccount(Double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+
+    public BusinessAccount() {
+    }
+
+    public Double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(Double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+
+    
+}
